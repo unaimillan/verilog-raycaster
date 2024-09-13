c8e3b6265531fdc40c999353bf1989c17c7e2b2c
Mike <m.kuskov@innopolis.university>
2024-09-13 13:10:02 +0300

    Update raycaster.sv

--- raycaster.sv
+++ raycaster.sv
@@ -71,13 +71,13 @@ typedef struct packed {
     uint8_t r;
 } color_t;
 
-function fix_t to_fix(input real real_num);
+function fix_t to_fix(input real_num);
     begin
         to_fix = fix_t'($rtoi(real_num * 2**16));
     end
 endfunction
 
-function real to_real(input fix_t fix_num);
+function to_real(input fix_t fix_num);
     begin
         to_real = $itor(fix_num) / 2**16;
     end
@@ -135,12 +135,11 @@ module raycaster (
     localparam TRIG_SAMPLES = `TRIG_SAMPLES;
     fix_t sin_table[TRIG_SAMPLES];
     fix_t sec_table[TRIG_SAMPLES];
-    generate
-        for(genvar i = 0; i < TRIG_SAMPLES; i++) begin
-            assign sin_table[i] = to_fix($sin(PI/2 * i/TRIG_SAMPLES));
-            assign sec_table[i] = to_fix(1/$cos(PI/2 * i/TRIG_SAMPLES));
-        end
-    endgenerate
+
+    initial begin
+        $readmemh("memory/sin_table.mem", sin_table);
+        $readmemh("memory/sec_table.mem", sec_table);
+    end
 
     function fix_t sin(input fix_t x);
         fix_t quad = mult(x, to_fix(2 / PI));
@@ -205,7 +204,7 @@ module raycaster (
 
     cell_t map [MAP_Y-1:0][MAP_X-1:0];
     initial begin
-        $readmemh(`LEVEL, map);
+        // $readmemh(`LEVEL, map);
     end
 
     function logic in_bounds(input vec_t pos);
@@ -234,59 +233,15 @@ module raycaster (
 
     texture_t textures[2:0];
 
-    function texture_t load_bmp (string path);
-        integer fd;
-        uint16_t signature, color_planes, bpp;
-        uint32_t data_offset, width, height;
-        texture_t texture;
-        begin
-            fd = $fopen(path, "rb");
-            $fread(signature, fd, 0);
-            if (signature != 16'h424d) begin
-                $display("image is not a bitmap");
-                $finish;
-            end
-            $fseek(fd, 32'ha, 0);
-            $fread(data_offset, fd);
-            data_offset = {<<8{data_offset}}; // reverse endianness
-
-            $fseek(fd, 32'h12, 0);
-            $fread(width, fd);
-            width = {<<8{width}};
-            $fread(height, fd);
-            height = {<<8{height}};
-            if (width != TEX_X || height != TEX_Y) begin
-                $display("image is must be %dx%d, found %dx%d.", TEX_X, TEX_Y,
-                width, height);
-                $finish;
-            end
-
-            $fread(color_planes, fd);
-            color_planes = {<<8{color_planes}};
-            if (color_planes != 1) begin
-                $display("image must have 1 color plane, found %d.", 
-                         color_planes);
-            end
-
-            $fread(bpp, fd);
-            bpp = {<<8{bpp}};
-            if (bpp != 24) begin
-                $display("image encoding must be 24-bit/pixel, found %d.", bpp);
-            end
-
-            $fseek(fd, data_offset, 0);
-            $fread(texture, fd);
-            $fclose(fd);
-            load_bmp = texture;
-        end
-    endfunction
+    texture_t texture_osaka;
+    assign textures[CELL_OSAKA-1] = texture_osaka;
 
     initial begin
-        textures[CELL_OSAKA-1] = load_bmp("textures/osaka.bmp");
-        textures[CELL_GRASS-1] = load_bmp("textures/grass.bmp");
-        textures[CELL_HUOHUO-1] = load_bmp("textures/huohuo.bmp");
+        $readmemh("memory/osaka.mem", texture_osaka);
+        $readmemh("memory/osaka.mem", textures[CELL_OSAKA-1]);
+        $readmemh("memory/grass.mem", textures[CELL_GRASS-1]);
+        $readmemh("memory/huohuo.mem", textures[CELL_HUOHUO-1]);
     end
-
 
     /* --------------------------- Movement --------------------------- */
 

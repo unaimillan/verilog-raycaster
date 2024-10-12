bf90b4d2f61ec00f80a15bb3f17e8db3afc3b92d
Mike Kuskov <m.kuskov@innopolis.university>
2024-10-13 00:18:20 +0300

    Add floating_to_fixed python conversion

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -1,19 +1,35 @@
 import ast
 from pathlib import Path
 import re
+from typing import Literal
 
 
-REGEX_PATTERN = re.compile(r'`to_fix\s?\((?<expression>.+?)\)')
+REGEX_PATTERN = re.compile(r'`to_fix\s?\((?P<expression>.+?)\)')
+
+
+def float_to_fixed(number: float, base: Literal['bin', 'dec', 'hex'] = 'bin') -> str:
+    fixed_number = int(number * 2 ** 16)
+    if base == 'bin':
+        result = f"'b{fixed_number:032b}"
+        result = result[:18] + '_' + result[18:]
+    elif base == 'dec':
+        result = f"'d{fixed_number}"
+    elif base == 'hex':
+        result = f"'h{fixed_number:08x}"
+        result = result[:6] + '_' + result[6:]
+    else:
+        raise ValueError(f'unknown base "{base}"')
+    return result
 
 
 def preprocess_line(line: str) -> str:
     result = []
     pos = 0
     for re_match in REGEX_PATTERN.finditer(line):
-        match_begin, match_end = re_match.span(0)
-        exp_begin, exp_end     = re_match.span(1)
         match_str = re_match.group(0)
         exp =  re_match.group(1)
+        match_begin, match_end = re_match.span(0)
+        exp_begin, exp_end     = re_match.span(1)
 
         # TODO: use ast.parse with definitions of the constants like PI, WIDTH, etc.
         evaluated = ast.literal_eval(exp)
